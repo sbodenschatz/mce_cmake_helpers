cc28e815378c24d0e2d65625e93c6a2e87045d3d
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2018-04-26 20:56:34 +0200

    Fixed possibly missing pthread for sanitizer instrumented builds (sanitizers need pthread).

--- SanitizerInstrumentationOption.cmake
+++ SanitizerInstrumentationOption.cmake
@@ -5,7 +5,7 @@ set(SANITIZER_INSTRUMENTATION OFF CACHE BOOL "Enable instrumentation with ASAN a
 if(SANITIZER_INSTRUMENTATION AND UNIX)
 	message(STATUS "Using sanitizer instrumentation.")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -fno-omit-frame-pointer -fsanitize=undefined")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address -fsanitize=undefined")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address -fsanitize=undefined -lpthread")
     if (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-sanitize=null")
     elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
