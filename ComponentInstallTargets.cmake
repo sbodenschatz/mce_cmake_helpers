7f503890d0a75baa0f7f5e99cc7a708175995331
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2018-05-02 19:40:35 +0200

    Allow overriding custom target dependencies in define_component_install_targets.

--- ComponentInstallTargets.cmake
+++ ComponentInstallTargets.cmake
@@ -5,9 +5,14 @@ include(CollectTargets)
 function(define_component_install_targets)
 	collect_default_targets_recursive(DIR_TARGETS)
 	foreach(COMP ${ARGN})
+		if(DEFINED INSTALL_COMPONENT_${COMP}_DEPENDS)
+			set(DEPS ${INSTALL_COMPONENT_${COMP}_DEPENDS})
+		else()
+			set(DEPS ${DIR_TARGETS})
+		endif()
 		add_custom_target(
 			install-${COMP}
-			DEPENDS ${DIR_TARGETS}
+			DEPENDS ${DEPS}
 			COMMAND
 				"${CMAKE_COMMAND}" -DCMAKE_INSTALL_COMPONENT=${COMP} -DCMAKE_INSTALL_CONFIG_NAME="$<CONFIG>" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_install.cmake"
 		)
